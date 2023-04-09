c5bf67a39078d127ced57a87ebd4ada7947de8a3
liuxminre <126866323+liuxminre@users.noreply.github.com>
2023-04-09 12:25:10 +0800

    Create Base 1.8 1.7 已经解决了并发问题，并且能支持 N 个 Segment 这么多次数的并发，但依然存在 HashMap 在 1.7 版本中的问题：查询遍历链表效率太低。因此 1.8 做了一些数据结构上的调整。    其中抛弃了原有的 Segment 分段锁，而采用了 CAS + synchronized 来保证并发安全性。  ConcurrentHashMap.java

---
+++ Base 1.8 1.7 已经解决了并发问题，并且能支持 N 个 Segment 这么多次数的并发，但依然存在 HashMap 在 1.7 版本中的问题：查询遍历链表效率太低。因此 1.8 做了一些数据结构上的调整。    其中抛弃了原有的 Segment 分段锁，而采用了 CAS + synchronized 来保证并发安全性。  ConcurrentHashMap.java
@@ -0,0 +1,35 @@
+final V putVal(K key, V value, boolean onlyIfAbsent) {
+    if (key == null || value == null) throw new NullPointerException();
+    int hash = spread(key.hashCode());
+    int binCount = 0;
+    for (Node<K,V>[] tab = table;;) {
+        Node<K,V> f; int n, i, fh;
+        if (tab == null || (n = tab.length) == 0)
+            tab = initTable();
+        else if ((f = tabAt(tab, i = (n - 1) & hash)) == null) {
+            if (casTabAt(tab, i, null,
+                            new Node<K,V>(hash, key, value, null)))
+                break;                   // no lock when adding to empty bin
+        }
+        else if ((fh = f.hash) == MOVED)
+            tab = helpTransfer(tab, f);
+        else {
+            V oldVal = null;
+            synchronized (f) {
+                if (tabAt(tab, i) == f) {
+                    if (fh >= 0) {
+                        binCount = 1;
+                        ···
+                    }
+                    else if (f instanceof TreeBin) {
+                       ···
+                    }
+                    else if (f instanceof ReservationNode)
+                        throw new IllegalStateException("Recursive update");
+                }
+            }
+            ···
+    }
+    addCount(1L, binCount);
+    return null;
+}
