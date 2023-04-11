973537268e88a3b171880bcbe1f3405bff0dccaf
liuxminre <126866323+liuxminre@users.noreply.github.com>
2023-04-11 18:49:46 +0800

    Create Window 的内部机制 Window 是一个抽象的概念，每一个 Window 对应着一个 View 和一个 ViewRootImpl。Window 实际是不存在的，它是以 View 的形式存在。对 Window 的访问必须通过 WindowManager，WindowManager 的实现类是 WindowManagerImpl：  WindowManagerImpl.java

---
+++ Window 的内部机制 Window 是一个抽象的概念，每一个 Window 对应着一个 View 和一个 ViewRootImpl。Window 实际是不存在的，它是以 View 的形式存在。对 Window 的访问必须通过 WindowManager，WindowManager 的实现类是 WindowManagerImpl：  WindowManagerImpl.java
@@ -0,0 +1,16 @@
+@Override
+public void addView(@NonNull View view, @NonNull ViewGroup.LayoutParams params) {
+    applyDefaultToken(params);
+    mGlobal.addView(view, params, mContext.getDisplay(), mParentWindow);
+}
+
+@Override
+public void updateViewLayout(@NonNull View view, @NonNull ViewGroup.LayoutParams params) {
+    applyDefaultToken(params);
+    mGlobal.updateViewLayout(view, params);
+}
+
+@Override
+public void removeView(View view) {
+    mGlobal.removeView(view, false);
+}
